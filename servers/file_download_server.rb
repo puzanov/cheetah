48df98b0f40508d8aa9d7911734913e9b00a2d3a
puzanov <puzanov@gmail.com>
2011-02-02 12:35:16 +0600

    threading added for download server

--- servers/file_download_server.rb
+++ servers/file_download_server.rb
@@ -15,27 +15,28 @@ loop do
   socket = server.accept
   while socket.gets.chop.length > 0
   end
- 
-  begin
-    @start_date = Time.new.to_i
-    socket.puts "HTTP/1.1 200 OK"
-    socket.puts "Content-type: application/octet-stream"
-    socket.puts "Content-Disposition: attachment; filename=\"zero.dat\"";
-    socket.puts "Content-length: " + FILESIZE.to_s
-    socket.puts ""
-    File::open(FILENAME_TO_DOWNLOAD, "rb") do |f|
-      f.each_chunk() {|chunk| socket.puts chunk }
+  Thread.new do
+    begin
+      @start_date = Time.new.to_i
+      socket.puts "HTTP/1.1 200 OK"
+      socket.puts "Content-type: application/octet-stream"
+      socket.puts "Content-Disposition: attachment; filename=\"zero.dat\"";
+      socket.puts "Content-length: " + FILESIZE.to_s
+      socket.puts ""
+      File::open(FILENAME_TO_DOWNLOAD, "rb") do |f|
+        f.each_chunk() {|chunk| socket.puts chunk }
+      end
+      socket.close
+    rescue
+      message_to_publish = "Закачка файла прервалась"  
+    else
+      @end_date = Time.new.to_i
+      seconds_spent = @end_date - @start_date
+      message_to_publish = SpeedCounter.new.calculate_speed FILESIZE, seconds_spent
+      message_to_publish = message_to_publish.to_s + " кб в секунду"
+    ensure
+      Publisher.new.publish message_to_publish  
     end
-    socket.close
-  rescue
-    message_to_publish = "Закачка файла прервалась"  
-  else
-    @end_date = Time.new.to_i
-    seconds_spent = @end_date - @start_date
-    message_to_publish = SpeedCounter.new.calculate_speed FILESIZE, seconds_spent
-    message_to_publish = message_to_publish.to_s + " кб в секунду"
-  ensure
-    Publisher.new.publish message_to_publish  
   end
 end
 
