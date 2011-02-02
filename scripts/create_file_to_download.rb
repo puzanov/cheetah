6648f0c89058719e0ab2add3d6a2cd4535110ba9
puzanov <puzanov@gmail.com>
2011-02-02 12:21:45 +0600

    added errors handler in download file server

--- scripts/create_file_to_download.rb
+++ scripts/create_file_to_download.rb
@@ -1,1 +1,1 @@
-exec "dd if=/dev/zero of=/tmp/hello bs=1M count=2"
+exec "dd if=/dev/zero of=/tmp/hello bs=1M count=#{ARGV[0]}"
--- servers/file_download_server.rb
+++ servers/file_download_server.rb
@@ -15,26 +15,28 @@ loop do
   socket = server.accept
   while socket.gets.chop.length > 0
   end
-  
-  @start_date = Time.new.to_i
-  
-  socket.puts "HTTP/1.1 200 OK"
-  socket.puts "Content-type: application/octet-stream"
-  socket.puts "Content-length: " + FILESIZE.to_s
-  socket.puts ""
-  
-  File::open(FILENAME_TO_DOWNLOAD, "rb") do |f|
-    f.each_chunk() {|chunk| socket.puts chunk }
+ 
+  begin
+    @start_date = Time.new.to_i
+    socket.puts "HTTP/1.1 200 OK"
+    socket.puts "Content-type: application/octet-stream"
+    socket.puts "Content-Disposition: attachment; filename=\"zero.dat\"";
+    socket.puts "Content-length: " + FILESIZE.to_s
+    socket.puts ""
+    File::open(FILENAME_TO_DOWNLOAD, "rb") do |f|
+      f.each_chunk() {|chunk| socket.puts chunk }
+    end
+    socket.close
+  rescue
+    message_to_publish = "Закачка файла прервалась"  
+  else
+    @end_date = Time.new.to_i
+    seconds_spent = @end_date - @start_date
+    message_to_publish = SpeedCounter.new.calculate_speed FILESIZE, seconds_spent
+    message_to_publish = message_to_publish.to_s + " кб в секунду"
+  ensure
+    Publisher.new.publish message_to_publish  
   end
-
-  socket.close
-  
-  @end_date = Time.new.to_i
-
-  seconds_spent = @end_date - @start_date
-  
-  speed = SpeedCounter.new.calculate_speed FILESIZE, seconds_spent
-  Publisher.new.publish speed
 end
 
 
