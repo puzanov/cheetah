c1e207e56f08b970ac68a34f0f7f866826c5dbaf
puzanov <puzanov@gmail.com>
2011-02-02 15:35:27 +0600

    empty config file added and jquery added

--- servers/file_download_server.rb
+++ servers/file_download_server.rb
@@ -17,24 +17,33 @@ loop do
   Thread.new do
     begin
       request = socket.gets
+      puts request
       uri = RequestParser.new.get_uri request
       @start_date = Time.new.to_i
-      socket.puts "HTTP/1.1 200 OK"
+      socket.puts "HTTP/1.0 200 OK"
       socket.puts "Content-type: application/octet-stream"
       socket.puts "Content-Disposition: attachment; filename=\"zero.dat\"";
       socket.puts "Content-length: " + FILESIZE.to_s
+      socket.puts "Connection: Keep-Alive"
       socket.puts ""
       File::open(FILENAME_TO_DOWNLOAD, "rb") do |f|
         f.each_chunk() {|chunk| socket.puts chunk }
       end
       socket.close
     rescue
+      puts $!
       message_to_publish = "Закачка файла прервалась"  
     else
       @end_date = Time.new.to_i
       seconds_spent = @end_date - @start_date
-      message_to_publish = SpeedCounter.new.calculate_speed FILESIZE, seconds_spent
-      message_to_publish = message_to_publish.to_s + " кб в секунду"
+      puts "start #{@start_date} end #{@end_date}"
+      speed = SpeedCounter.new.calculate_speed FILESIZE, seconds_spent
+      if speed > 1024
+        speed = speed / 1024
+        message_to_publish = speed.to_s + " мегабайт в секунду"
+      else
+        message_to_publish = speed.to_s + " кб в секунду"
+      end
     ensure
       Publisher.new.publish message_to_publish, uri 
     end
