fe1f5c3a5f6ecb06117aeff7fcc8a93a52d4fbdc
puzanov <puzanov@gmail.com>
2011-02-04 23:02:25 +0600

    changed http get method

--- lib/namba_login.rb
+++ lib/namba_login.rb
@@ -1,29 +1,38 @@
 class NambaLogin
-  require 'rubygems'
-  require 'httparty'
-  include HTTParty
-  format :html
-
+  require 'net/http'
+  require 'uri'
+  
   def NambaLogin.get_dev_namba_url
-    "http://namba.dev/username.php"
+    "http://namba.dev/username.php?s="
   end
 
   def NambaLogin.get_kg_namba_url
-    "http://namba.kg/username.php"
+    "http://namba.kg/username.php?s="
   end
+  
+  def NambaLogin.get_content(requested_url)
+    url = URI.parse(requested_url)
+    full_path = "#{url.path}?#{url.query}"
+    the_request = Net::HTTP::Get.new(full_path)
+
+    the_response = Net::HTTP.start(url.host, url.port) { |http|
+      http.request(the_request)
+    }
+
+    raise "Response was not 200, response was #{the_response.code}" if the_response.code != "200"
+    return the_response.body
+  end 
 end
 
 class NambaLoginKG
   def NambaLoginKG.get_login session_id
-    options = { :query => {:s => session_id}}
-    NambaLogin.get(NambaLogin.get_kg_namba_url, options)    
+    NambaLogin.get_content NambaLogin.get_kg_namba_url + session_id   
   end
 end
 
 class NambaLoginDEV
   def NambaLoginDEV.get_login session_id
-    options = { :query => {:s => session_id}}
-    NambaLogin.get(NambaLogin.get_dev_namba_url, options)    
+    NambaLogin.get_content NambaLogin.get_dev_namba_url + session_id   
   end
 end
 
--- test/namba_login_test.rb
+++ test/namba_login_test.rb
@@ -4,7 +4,7 @@ require 'lib/namba_login'
 class NambaLoginTest < Test::Unit::TestCase
   def test_get_login
     session_id = "rc1kj3upp340omaj3qbb3c68i1"
-    login = NambaLoginDEV.get_login session_id
+    login = NambaLoginKG.get_login session_id
     puts login
   end
 end
